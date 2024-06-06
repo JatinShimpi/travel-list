a2a8589222f79efa636492aa1f64006287415c96
JatinShimpi <shimpijatin184@gmail.com>
2024-06-06 16:39:15 +0530

    added form sumbit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "charger", quantity: 1, packed: true },
+  { id: 3, description: "charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -19,12 +21,41 @@ function Logo() {
   return <h1>🌴far away👜</h1>;
 }
 function Form() {
+  const [description, setDisciprtion] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) {
+      return;
+    }
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDisciprtion("");
+    setQuantity('');
+  }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do you need for the 😍 trip</h3>
-      <select>
-        {Array.from({length:20},(_,i)=>i+1)}
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDisciprtion(e.target.value)}
+      ></input>
+      <button>Add</button>
     </form>
   );
 }
@@ -33,7 +64,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
